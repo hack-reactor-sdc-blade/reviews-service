1be38d1c354627aeacaacb8a2928d9c4a476077f
Andrew Schuessler <ajschuessler@gmail.com>
2019-04-26 11:47:24 -0400

    added expressStaticGzip to serve up compressed static files

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressStaticGzip = require('express-static-gzip');
 const morgan = require('morgan');
 const path = require('path');
 const util = require('util');
@@ -8,7 +9,15 @@ const port = process.env.PORT || 3002;
 const { getReviewsFromDatabase, getSearchResultsFromDatabase } = require('../database/helper/helpers.js');
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
+  enableBrotli: true,
+  customCompressions: [{
+      encodingName: 'deflate',
+      fileExtension: 'zz'
+  }],
+  orderPreference: ['br']
+}));
 app.use(cors());
 
 
