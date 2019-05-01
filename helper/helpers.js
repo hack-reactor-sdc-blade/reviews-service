c82f46bf6b0d9fcefd816df463bfdffba66c29ef
Andrew Schuessler <ajschuessler@gmail.com>
2019-05-01 14:13:55 -0400

    updated mysql connections to pooling

--- helper/helpers.js
+++ helper/helpers.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 var configWithDb = require('../configWithDb.js');
-var connection = mysql.createConnection(configWithDb);
-connection.connect();
+var pool = mysql.createPool(configWithDb);
+// connection.connect();
 
 
 const getReviewsFromDatabase = (id, callback) => {
-  connection.query(`SELECT users.name, users.avatar, reviews.date, reviews.text, reviews.rating, reviews.has_response, reviews.owner_response FROM users, reviews WHERE users.id = reviews.user_id AND reviews.apartment_id = ${id};`, (err, result) => {
+  pool.query(`SELECT users.name, users.avatar, reviews.date, reviews.text, reviews.rating, reviews.has_response, reviews.owner_response FROM users, reviews WHERE users.id = reviews.user_id AND reviews.apartment_id = ${id};`, (err, result) => {
     if (err) {
         console.log(err);
     } else {
@@ -15,7 +15,7 @@ const getReviewsFromDatabase = (id, callback) => {
 }
 
 const getSearchResultsFromDatabase = (id, word, callback) => {
-  connection.query(`SELECT users.name, users.avatar, reviews.date, reviews.text, reviews.rating, reviews.has_response, reviews.owner_response FROM users, reviews WHERE users.id = reviews.user_id AND reviews.apartment_id = ${id} AND (reviews.text LIKE '%${word}%' OR reviews.text LIKE '% ${word}%');`, (err, result) => {
+  pool.query(`SELECT users.name, users.avatar, reviews.date, reviews.text, reviews.rating, reviews.has_response, reviews.owner_response FROM users, reviews WHERE users.id = reviews.user_id AND reviews.apartment_id = ${id} AND (reviews.text LIKE '%${word}%' OR reviews.text LIKE '% ${word}%');`, (err, result) => {
     if (err) {
         console.log(err);
     } else {
