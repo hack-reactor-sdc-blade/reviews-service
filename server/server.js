cef658aebc25e8db2605a13349fdf6762e5f17de
Andrew Schuessler <ajschuessler@gmail.com>
2019-05-02 16:41:01 -0400

    updated aws addresses in client, updated server to remove unused code

--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ const util = require('util');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3002;
-const axios = require('axios');
-var http = require('http');
-var https = require('https');
+// var http = require('http');
+// var https = require('https');
 const { getReviewsFromDatabase, getSearchResultsFromDatabase } = require('../helper/helpers.js');
 
-http.globalAgent.maxSockets = 25;
-https.globalAgent.maxSockets = 25;
+// http.globalAgent.maxSockets = 25;
+// https.globalAgent.maxSockets = 25;
 
 app.use(morgan('dev'));
+
 // app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
   enableBrotli: true,
@@ -24,6 +24,7 @@ app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
   }],
   orderPreference: ['br']
 }));
+
 app.use(cors());
 
 
@@ -43,59 +44,31 @@ app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-// app.get('/room/:id', (req, res) => {
-//   axios.get(`http://ec2-54-172-211-135.compute-1.amazonaws.com/room/${req.params.id}`)
-//   .then(results => res.send(results.data))
-//   .catch(err => {console.log(err)})
-// })
-
-getPaginatedItems = (items, offset) => {
-  return items.slice(offset, offset + 7);
-}
-
-sortReviews = dates => {
-  return dates.sort((a, b) => {
-    const dateA = new Date(a.date.replace(' ', ', '));
-    const dateB = new Date(b.date.replace(' ', ', '));
-    return dateB - dateA;
-  });
-}
-
 app.get('/room/:id', (req, res) => {
   getReviewsFromDatabase(req.params.id, (err, data) => {
     if (err) {
       console.error('Error retrieving reviews from database', err);
     } else {
-      // let items = sortReviews(data);
-      // let offset = req.query.offset ? parseInt(req.query.offset) : 0;
-      // let nextOffset = offset + 7;
-      // let previousOffset = offset - 7 < 1 ? 0 : offset - 7;
-      // let meta = {
-      //   limit: 7,
-      //   next: util.format('?limit=%s&offset=%s', 7, nextOffset),
-      //   offset: req.query.offset,
-      //   previous: util.format('?limit=%s&offset=%s', 7, previousOffset),
-      //   total_count: items.length,
-      // };
-      // let json = {
-      //   meta: meta,
-      //   comments: getPaginatedItems(items, offset),
-      //   data: data
-      // };
-      // return res.json(json);
-      res.send(data);
+      let items = sortReviews(data);
+      let offset = req.query.offset ? parseInt(req.query.offset) : 0;
+      let nextOffset = offset + 7;
+      let previousOffset = offset - 7 < 1 ? 0 : offset - 7;
+      let meta = {
+        limit: 7,
+        next: util.format('?limit=%s&offset=%s', 7, nextOffset),
+        offset: req.query.offset,
+        previous: util.format('?limit=%s&offset=%s', 7, previousOffset),
+        total_count: items.length,
+      };
+      let json = {
+        meta: meta,
+        comments: getPaginatedItems(items, offset),
+        data: data
+      };
+      return res.json(json);
     }
   });
 });
-
-
-
-
-// app.get('/:id/search/:word', (req, res) => {
-//   axios.get(`http://ec2-54-172-211-135.compute-1.amazonaws.com/${req.params.id}/search/${req.params.word}`)
-//   .then(results => res.send(results.data))
-//   .catch(err => console.log(err))
-// });
 
 
 app.get('/:id/search/:word', (req, res) => {
