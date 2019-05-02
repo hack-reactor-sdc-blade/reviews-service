68efa99f9ffae12b493deda7f6e6ceed369e17d6
Andrew Schuessler <ajschuessler@gmail.com>
2019-05-02 16:13:57 -0400

    removed server side data manipulation of db query responses to test speed improvement

--- server/server.js
+++ server/server.js
@@ -66,23 +66,24 @@ app.get('/room/:id', (req, res) => {
     if (err) {
       console.error('Error retrieving reviews from database', err);
     } else {
-      let items = sortReviews(data);
-      let offset = req.query.offset ? parseInt(req.query.offset) : 0;
-      let nextOffset = offset + 7;
-      let previousOffset = offset - 7 < 1 ? 0 : offset - 7;
-      let meta = {
-        limit: 7,
-        next: util.format('?limit=%s&offset=%s', 7, nextOffset),
-        offset: req.query.offset,
-        previous: util.format('?limit=%s&offset=%s', 7, previousOffset),
-        total_count: items.length,
-      };
-      let json = {
-        meta: meta,
-        comments: getPaginatedItems(items, offset),
-        data: data
-      };
-      return res.json(json);
+      // let items = sortReviews(data);
+      // let offset = req.query.offset ? parseInt(req.query.offset) : 0;
+      // let nextOffset = offset + 7;
+      // let previousOffset = offset - 7 < 1 ? 0 : offset - 7;
+      // let meta = {
+      //   limit: 7,
+      //   next: util.format('?limit=%s&offset=%s', 7, nextOffset),
+      //   offset: req.query.offset,
+      //   previous: util.format('?limit=%s&offset=%s', 7, previousOffset),
+      //   total_count: items.length,
+      // };
+      // let json = {
+      //   meta: meta,
+      //   comments: getPaginatedItems(items, offset),
+      //   data: data
+      // };
+      // return res.json(json);
+      res.send(data);
     }
   });
 });
